d22cbf88ab60d070eedddb7ea51795ac7b7acff4
Xi Zhao <zhao.xi3@northeastern.edu>
2021-04-05 15:53:35 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+  res.send({hi: 'there'})
+});
+
+// app.get('/', function(req,res){
+//   res.send({hi: 'there'})
+// });
+
+const PORT = process.env.PORT || 5000; //Dynamic port
+app.listen(PORT);
