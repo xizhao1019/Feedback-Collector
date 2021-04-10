c75445a9916431564f9e57351fe03aa219663134
Xi Zhao <zhao.xi3@northeastern.edu>
2021-04-09 23:44:47 -0400

    auth-google

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
-
-
-const app = express();
-
-require('./routes/authRoutes')(app);
 
 mongoose.connect(keys.mongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
   });
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 app.get('/', (req,res)=>{
   res.send({hello: 'kevin'})
